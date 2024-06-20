b2cf2ea51f3034f20d4444c3eb46153271c6a263
mwihoti <danielmwihoti@gmail.com>
2024-06-20 21:53:33 +0300

    SignIn form and state

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -7,7 +7,8 @@ const SignIn = () => {
         firstname: '',
         lastname: '',
         email: '',
-        password: ''
+        password: '',
+         password2: ''
     })
 
     const handleChange = (e) => {
@@ -19,34 +20,52 @@ const SignIn = () => {
 
   return (
     <div>
-        <form className='grid grid-row items-center p-4 border border-black m-2'>
-            <label >
+
+    
+    <div className=' flex  justify-center m-32'>
+        <form className='grid grid-row items-center justify-center h-3/4 p-4 border w-1/2  '>
+            <label className='items-center ' >
+                <h3>
                 <strong>FirstName</strong>
-                <input type="text"  name='firstname' value={data.firstname} onChange={handleChange} required />
+                </h3>
+                <input type="text"  name='firstname' value={data.firstname} className='border m-2 p-2 border-black rounded' onChange={handleChange} required />
 
             </label>
 
-            <label>
+            <label className=' justify-center'>
+                <h3>
                 <strong>LastName:</strong>
-                <input type='text' name='lastname' value={data.lastname} onChange={handleChange}  required />
+                </h3>
+               
+                <input type='text' name='lastname' value={data.lastname} className='border m-2 p-2 border-black rounded' onChange={handleChange}  required />
             </label>
 
-            <label>
+            <label className='items-center'>
+                <h3>
                 <strong>Email:</strong>
-                <input type='text' name='email' value={data.email} onChange={handleChange}  required />
+                </h3>
+               
+                <input type='text' name='email' value={data.email} className='border m-2 p-2 border-black rounded' onChange={handleChange}  required />
             </label>
 
-            <label>
-                <strong>Password:</strong>
-                <input type='password' name='password' value={data.password} onChange={handleChange}  required />
+            <label className='items-center '>
+                <h3> <strong>Password:</strong></h3>
+               
+                <input type='password' name='password' value={data.password} className='border m-2 p-2 border-black rounded' onChange={handleChange}  required />
             </label>
 
-            <label>
-                <strong>Re-enter Password:</strong>
-                <input type='password' name='password' value={data.password} onChange={handleChange}  required />
+            <label className='items-center'>
+                <h3><strong>Re-enter Password:</strong></h3>
+                
+                <input type='password' name='password2' value={data.password2} className='border m-2 p-2 border-black rounded' onChange={handleChange}  required />
             </label>
+
+            <button type='submit' className='bg-gray-300 p-2 m-3 hover:bg-stone-950 hover:text-white rounded-2xl'>
+                SignIn
+            </button>
 
         </form>
+    </div>
     </div>
   )
 }
