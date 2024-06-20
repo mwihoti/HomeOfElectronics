bd9d581393b717215f926ac8df5ad12e48ad5244
mwihoti <danielmwihoti@gmail.com>
2024-06-20 16:05:24 +0300

    Signin form

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,37 @@
 import React from 'react'
 
+
 const SignIn = () => {
+    
   return (
     <div>
         <form>
-            
+            <label >
+                <strong>FirstName</strong>
+                <input type="text" name='firstname' required />
+
+            </label>
+
+            <label>
+                <strong>LastName:</strong>
+                <input type='text' name='lastname' required />
+            </label>
+
+            <label>
+                <strong>Email:</strong>
+                <input type='text' name='email' required />
+            </label>
+
+            <label>
+                <strong>Password:</strong>
+                <input type='password' name='password' required />
+            </label>
+
+            <label>
+                <strong>Re-enter Password:</strong>
+                <input type='password' name='password' required />
+            </label>
+
         </form>
     </div>
   )
