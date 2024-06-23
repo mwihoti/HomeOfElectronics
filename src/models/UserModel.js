4bdcfa5ddb98295af5780033b0d65acbcbb7333b
mwihoti <danielmwihoti@gmail.com>
2024-06-23 17:10:47 +0300

    Created userModel

---
+++ src/models/UserModel.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserModel = new mongoose.Schema({
+    username: {
+        name: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        name: String,
+        required: true,
+        unique: true,
+        match: [/.+\@.+\..}/, 'Please enter a valid email address']
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+
+},
+{ timestamps: true
+
+});
+
+UserModel.pre('save', async function(next) {
+    if (!this.isModified('password')){
+        next();
+
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next ();
+
+
+
+})
+
+UserModel.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+export default mongoose.models.User || mongoose.model('User', UserModel);
+
+
